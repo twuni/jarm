2495844add5fd9f9aca8bdb9f634e7df4c8b256a
Devin Canterberry <devin@canterberry.cc>
2019-01-29 11:51:26 -0800

    🏁 v0.4.3 Release
    `Store#listResources()` now returns full copies of resources it returns.
    This comes with a significant performance cost -- O(n²) queries vs O(1),
    so this function now takes an optional second options parameter with the
    following properties:
    
     * `shallow` (boolean) - If `true`, then returned resources will be shallow
       copies, with no `relationships` property. This option should be used when
       relationships are not needed and will result in faster performance.
    
     * `limit` (number) - Limit the number of records returned.

--- src/store/list-resources/index.js
+++ src/store/list-resources/index.js
@@ -3,6 +3,25 @@ import buildValues from './build-values';
 import buildWhereClause from './build-where-clause';
 import toProcessableType from '../../helpers/to-processable-type';
 
+const toResourceRelationship = (definition, records) => {
+  const relationships = records.map((record) => definition.columns.reduce((attributes, { attribute, column }) => {
+    attributes[attribute] = record[column];
+    return attributes;
+  }, {}));
+
+  return relationships;
+};
+
+const toResourceRelationships = (definitions, recordSets) => definitions.reduce((relationships, definition, index) => {
+  const { [index]: records } = recordSets;
+  if (records) {
+    relationships[definition.name] = {
+      data: toResourceRelationship(definition, records)
+    };
+  }
+  return relationships;
+}, {});
+
 const buildStatement = (schema, query) => {
   const joins = buildJoinClause(schema, query);
   const selection = buildWhereClause(schema, query);
@@ -18,16 +37,35 @@ const buildStatement = (schema, query) => {
   return `SELECT ${projection} FROM ${schema.table}${joins}${selection}`;
 };
 
-const listResources = (schema) => (query = {}) => async (read) => {
-  const records = await read(buildStatement(schema, query), buildValues(schema, query).map(toProcessableType));
+const defaultOptions = {
+  limit: Infinity,
+  shallow: false
+};
 
-  return records.map((record) => ({
+const listResources = (schema) => (query = {}, {
+  limit = defaultOptions.limit,
+  shallow = defaultOptions.shallow
+} = defaultOptions) => async (read) => {
+  const records = await read(buildStatement(schema, query), buildValues(schema, query).map(toProcessableType));
+  const resources = records.map((record) => ({
     attributes: schema.columns.reduce((attributes, { attribute, column }) => {
       attributes[attribute] = record[column];
       return attributes;
     }, {}),
     id: record[schema.id.name],
     type: schema.resource
+  })).slice(0, limit);
+
+  if (shallow) {
+    return resources;
+  }
+
+  return Promise.all(resources.map(async (resource) => {
+    const relationships = await Promise.all(schema.relationships.map((relationship) => read(`SELECT ${relationship.columns.map(({ column }) => column).join(', ')} FROM ${relationship.table} WHERE ${schema.id.name} = $1`, [resource.id])));
+
+    resource.relationships = toResourceRelationships(schema.relationships, relationships);
+
+    return resource;
   }));
 };
 
--- src/store/retrieve-resource/index.js
+++ src/store/retrieve-resource/index.js
@@ -1,35 +1,7 @@
 import listResources from '../list-resources';
 
-const toResourceRelationship = (definition, records) => {
-  const relationships = records.map((record) => definition.columns.reduce((attributes, { attribute, column }) => {
-    attributes[attribute] = record[column];
-    return attributes;
-  }, {}));
-
-  return relationships;
-};
-
-const toResourceRelationships = (definitions, recordSets) => definitions.reduce((relationships, definition, index) => {
-  const { [index]: records } = recordSets;
-  if (records) {
-    relationships[definition.name] = {
-      data: toResourceRelationship(definition, records)
-    };
-  }
-  return relationships;
-}, {});
-
 const retrieveResource = (schema) => (query = {}) => async (read) => {
-  const [resource] = await listResources(schema)(query)(read);
-
-  if (!resource) {
-    return resource;
-  }
-
-  const relationships = await Promise.all(schema.relationships.map((relationship) => read(`SELECT ${relationship.columns.map(({ column }) => column).join(', ')} FROM ${relationship.table} WHERE ${schema.id.name} = $1`, [resource.id])));
-
-  resource.relationships = toResourceRelationships(schema.relationships, relationships);
-
+  const [resource] = await listResources(schema)(query, { limit: 1 })(read);
   return resource;
 };
 
